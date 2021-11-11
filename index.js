be70ea92aa8952996a5dbbfa76f9953371bec837
nepolionchakma <nepolionchakma.nc@gmail.com>
2021-11-11 08:07:30 +0600

    commit

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
-const { MongoClient } = require('mongodb');
 const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
+const { MongoClient } = require('mongodb');
 require("dotenv").config();
-
-const port = process.env.PORT || 5000;
 
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9zut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
