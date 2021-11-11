ef0edda35c6753fb3a819178edc63eef506c8e4b
nepolionchakma <nepolionchakma.nc@gmail.com>
2021-11-11 11:49:45 +0600

    commit

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ async function run() {
             const filter = { email: user.email };
             const options = { upsert: true };
             const updateDoc = { $set: user };
-            const result = userCollection.updateOne(filter, updateDoc, options);
+            const result = await userCollection.updateOne(filter, updateDoc, options);
             res.json(result)
         })
 
