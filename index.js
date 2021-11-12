83e5589adfe7b3d60cb8914ce798dda212ca57ac
nepolionchakma <nepolionchakma.nc@gmail.com>
2021-11-12 16:58:57 +0600

    commit

--- index.js
+++ index.js
@@ -134,6 +134,13 @@ async function run() {
             const reviews = await cursor.toArray();
             res.send(reviews)
         })
+        // order delete data
+        app.delete("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result)
+        })
 
     }
     finally {
